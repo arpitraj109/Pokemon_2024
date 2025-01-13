a724c46fce700ae5639acb87e16f4738b154ea42
arpitraj109 <93399431+arpitraj109@users.noreply.github.com>
2025-01-13 16:40:47 +0530

    Update PokemonCard.js
    card more defined

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -2,28 +2,30 @@ import React from "react";
 import "./PokemonCard.css";
 
 const PokemonCard = ({ name, image, types = [], stats = [] }) => {
-    return (
-      <div className="pokemon-card">
-        <img src={image} alt={name} className="pokemon-image" />
-        <h3 className="pokemon-name">{name}</h3>
-        <div className="pokemon-types">
-          {types.map((type, index) => (
-            <span key={index} className={`type-badge type-${type}`}>
-              {type}
-            </span>
-          ))}
-        </div>
-        <div className="pokemon-stats">
-          {stats.map((stat, index) => (
-            <div key={index} className="stat">
-              <span className="stat-name">{stat.name}:</span>
-              <span className="stat-value">{stat.value}</span>
-            </div>
-          ))}
-        </div>
+  // Dynamically set the card background based on the primary type
+  const primaryType = types[0] || "normal"; // Default to "normal" if no type is provided
+
+  return (
+    <div className={`pokemon-card type-${primaryType}`}>
+      <img src={image} alt={name} className="pokemon-image" />
+      <h3 className="pokemon-name">{name}</h3>
+      <div className="pokemon-types">
+        {types.map((type, index) => (
+          <span key={index} className={`type-badge type-${type}`}>
+            {type}
+          </span>
+        ))}
       </div>
-    );
-  };
-  
+      <div className="pokemon-stats">
+        {stats.map((stat, index) => (
+          <div key={index} className="stat">
+            <span className="stat-name">{stat.name}:</span>
+            <span className="stat-value">{stat.value}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PokemonCard;
